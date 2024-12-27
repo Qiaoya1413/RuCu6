f8f9c0b02d6ed898013fe3939fe0c0d10f30b3e5
Qiao <101532293+Qiaoya1413@users.noreply.github.com>
2024-12-28 07:58:48 +0800

    Update header.js

--- Scripts/header.js
+++ Scripts/header.js
@@ -1,12 +1,12 @@
+// Loon 专用脚本
 const url = $request.url;
 const header = $request.headers;
 const contype = header["Content-Type"] || header["content-type"];
-const isQuanX = typeof $task !== "undefined";
 
 if (url.includes("/sec.sginput.qq.com/q")) {
   // 搜狗输入法候选词推广拦截
   if (contype === "application/octet-stream") {
-    $done(isQuanX ? { status: "HTTP/1.1 404 Not Found" } : {});
+    $done({ status: "HTTP/1.1 404 Not Found" });
   } else {
     $done({});
   }
